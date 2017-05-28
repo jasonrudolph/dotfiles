fe2ea4642b4fc68a20f164b37d9e061903d204d9
Jason Rudolph <jason@jasonrudolph.com>
2017-05-27 21:40:39 -0400

    Expose URL for toggling Wi-Fi on/off

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,6 +26,17 @@ end
 postureReminderTimer = hs.timer.doAt('00:00', '15m', showPostureReminder)
 
 --------------------------------------------------------------------------------
+-- Bind URL for toggling the Wi-Fi on/off.
+--
+--   hammerspoon://toggle-wifi
+--------------------------------------------------------------------------------
+function toggleWiFI()
+  local isOn = hs.wifi.interfaceDetails('en0')['power']
+  hs.wifi.setPower(not isOn)
+end
+hs.urlevent.bind("toggle-wifi", toggleWiFI)
+
+--------------------------------------------------------------------------------
 -- Bind URL for toggling the macOS "Do Not Disturb" setting:
 --
 --   hammerspoon://toggle-do-not-disturb
